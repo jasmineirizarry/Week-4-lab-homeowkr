c0692480ee2a4418f24a250c71dbcb7893cec81c
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-10-27 20:31:24 -0500

    Add files via upload

---
+++ M4P4.py
@@ -0,0 +1,18 @@
+#jasmine irizarry
+#CSS225 week 4 lab
+#October 25th, 2020
+
+
+
+
+def year '1900'
+    if year ==1900 and <==2020
+print("Woah, that's the past!")
+
+
+def year '2020'
+    if year >==2000 and <==2020
+print ("that's totally the present")
+
+
+
