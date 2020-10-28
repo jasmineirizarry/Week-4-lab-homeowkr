b707a714eacf4d087460cbfe82c58eb3c183caf7
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-10-27 20:33:14 -0500

    Add files via upload

---
+++ M4P2.py
@@ -0,0 +1,16 @@
+#jasmine irizarry
+#CSS225 week 4 lab
+#October 25th, 2020
+
+
+input("Hello, possible pirate! What's the password")
+print("The password is none of your business!")
+print("Go away, pirate")
+
+
+
+
+
+input("Greetings, hater of pirates")
+print("I am not a hater, the password is sacred")
+
