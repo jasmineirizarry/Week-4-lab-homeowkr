6ab871c7cd1f0e380672af2ae74c8a000df584c5
jasmineirizarry <72569201+jasmineirizarry@users.noreply.github.com>
2020-10-27 19:40:41 -0500

    Add files via upload

---
+++ M4P1.py
@@ -0,0 +1,16 @@
+#jasmine irizarry
+#CSS225 week 4 lab
+#October 25th, 2020
+
+
+
+input("what is the currenty time")
+print("19:37")
+
+
+
+input("how many hours do you want to wait?")
+print("2 more hours")
+
+print("21:37 is the final time")
+
---
+++ M4P5.py
@@ -0,0 +1,29 @@
+#jasmine irizarry
+#CSS225 week 4 lab
+#October 25th, 2020
+
+#Input: number between 0 and 100
+#output: letter grade (A,B,C,D,F)
+
+def find_grade (number_grade):
+    #if the student has a grade between 90 and 100, she/he gets an A
+    if number_grade >=90 and number_grade <= 100:
+        return "A"
+print("Student is passing")
+
+
+
+def find_grade (number_grade):
+    #if my number is lower than 60, she/he gets an F
+    if number_grade >=0 and number_grade <=60
+return "F"
+print("Student is failing")
+
+
+
+
+
+
+
+
+
